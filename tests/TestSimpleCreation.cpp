2b495c1a886c0b526f15b94a60f5a6bd4a3a2934
Jared Carlson <jcarlson@gototheboard.com>
2017-06-22 19:53:59 -0400

    Adding the structure for the tests

---
+++ tests/TestSimpleCreation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "DTGraph.h"
+#include "DTGraph/GenericGraph.h"
+#include "DTGraph/Node.h"
+#include "DTGraph/Edge.h"
+
+/*
+ * Declare our types
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+}
