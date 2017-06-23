6b4523b0337acc7c30238da76d36bffced657cda
Jared Carlson <jcarlson@gototheboard.com>
2017-06-23 14:09:24 -0400

    Well, can't believe it compiled that easily...

--- tests/TestSimpleCreation.cpp
+++ tests/TestSimpleCreation.cpp
@@ -8,8 +8,39 @@
  * Declare our types
  */
 
+class SimpleNode;
+
+typedef DTEdge<SimpleNode,int> SimpleEdgeTy;
+
+class SimpleEdge : public SimpleEdgeTy {
+  
+};
+
+
+typedef DTNode<SimpleNode,SimpleEdge> SimpleNodeTy;
+class SimpleNode : public SimpleNodeTy {
+  
+};
+
+typedef DTGraph<SimpleNode,SimpleEdge> SimpleGraphTy;
+class SimpleGraph : public SimpleGraphTy {
+
+};
+
+/*
+ * Main
+ */
 int main(int argc, char *argv[])
 {
+  
+  std::cout << "Exercising Simple Creation test" << std::endl;
 
+  SimpleGraph * simpleGraph;
+
+  std::cout << "Created a bare bones (empty) simple graph" << std::endl;
+  
+  delete simpleGraph;
+
+  return 0;
 
 }
