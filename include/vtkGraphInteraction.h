fe987f63b8cf6e7c9d5e80e7ffb938a9d1081825
Jared Carlson <jcarlson@gototheboard.com>
2017-07-01 22:33:27 -0400

    Starting to add the glue code to take a DTGraph and translate it to a vtkGraph

---
+++ include/vtkGraphInteraction.h
@@ -0,0 +1,53 @@
+#ifndef _VTK_GRAPH_INTERACTION_MODEL_H
+#define _VTK_GRAPH_INTERACTION_MODEL_H
+
+#include "DTGraph.h"
+
+#include "vtkActor.h"
+#include "vtkDataSetAttributes.h"
+#include "vtkGraphLayoutView.h"
+#include "vtkMutableDirectedGraph.h"
+#include "vtkRandomGraphSource.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkStringArray.h"
+#include "vtkTree.h"
+#include "vtkViewTheme.h"
+#include "vtkGlyph2D.h"
+#include "vtkGlyphSource2D.h"
+#include "vtkPolyData.h"
+#include "vtkSmartPointer.h"
+#include "vtkInteractorStyleRubberBand2D.h"
+#include "vtkSelection.h"
+#include "vtkSelectionNode.h"
+#include "vtkDataRepresentation.h"
+#include "vtkAnnotationLink.h"
+#include "vtkRenderedGraphRepresentation.h"
+
+template<class NodeTy, class EdgeTy> 
+class vtkGraphInteractionModel {
+  
+ public:
+  vtkGraphInteractionModel() {
+    dGraph = nullptr;
+  }
+  
+  ~vtkGraphInteractionModel() {
+    releaseGraph();
+  }
+
+  void setGraph(DTGraph<NodeTy,EdgeTy> * g) { dGraph = g; }
+  void releaseGraph(void) { dGraph = nullptr; }
+  
+  vtkMutableDirectedGraph* generateVtkGraph(void) {
+    return nullptr;
+  }
+  
+
+ private:
+  DTGraph<NodeTy,EdgeTy>* dGraph;
+
+};
+
+#endif
---
+++ src/vtkGraphInteraction.cpp
@@ -0,0 +1,1 @@
+#include "vtkGraphInteraction.h"
